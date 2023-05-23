e8ebe2a0052f357374beb7b5396e9882b5f07fdc
Hari Narasimhan <harin76@gmail.com>
2023-05-23 14:52:20 +0530

    fix: updated_at issue

--- packages/knex-repository/lib/index.js
+++ packages/knex-repository/lib/index.js
@@ -52,7 +52,7 @@ class Repository {
 
   async update({ db, params, payload }) {
     // Set the updated_at attribute
-    payload.updated_at = new Date()
+    payload.updatedAt = new Date()
     await db(this.table).where({ id: params.id }).update(payload)
     return this.findById({ db, params })
   }
